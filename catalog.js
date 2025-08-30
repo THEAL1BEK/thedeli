96b7e7a04dfde22d6b8aa1ed9ed7abf9f023e4ae
THEKURDEL1 <qypshaq_all@mail.ru>
2025-08-30 10:24:23 +0600

    Create catalog.js

---
+++ catalog.js
@@ -0,0 +1,59 @@
+const products = [
+    {
+        name: "Ivory Sunglasses",
+        price: 99,
+        image: "ivory.jpg",
+        gender: "men",
+        winter: true,
+        newIn: false
+    },
+    {
+        name: "Lalo Sunglasses",
+        price: 129,
+        image: "IMG_8125.JPG",
+        gender: "women",
+        winter: false,
+        newIn: true
+    },
+    {
+        name: "Rod Sunglasses",
+        price: 89,
+        image: "IMG_8125.JPG",
+        gender: "men",
+        winter: true,
+        newIn: false
+    },
+    {
+        name: "Brock Sunglasses",
+        price: 149,
+        image: "IMG_8125.JPG",
+        gender: "women",
+        winter: false,
+        newIn: false
+    },
+    {
+        name: "Glock Sunglasses",
+        price: 109,
+        image: "IMG_8125.JPG",
+        gender: "men",
+        winter: true,
+        newIn: true
+    },
+    {
+        name: "Leck Sunglasses",
+        price: 119,
+        image: "IMG_8125.JPG",
+        gender: "women",
+        winter: false,
+        newIn: false
+    }
+    // Добавляй новые товары сюда, например:
+    // {
+    //     name: "New Product",
+    //     price: 150,
+    //     image: "new_image.jpg",
+    //     gender: "women",
+    //     winter: true,
+    //     newIn: true
+    // }
+];
