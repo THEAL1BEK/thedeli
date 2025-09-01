8bdcb2608ab6e165ae2a6348ba63bf917f268d81
THEKURDEL1 <qypshaq_all@mail.ru>
2025-09-01 17:42:23 +0600

    Create products.js

---
+++ products.js
@@ -0,0 +1,8 @@
+const products = [
+    { name: "Cat Eye Glasses", price: 149, images: ["IMG_8125.JPG", "IMG_8125.JPG", "IMG_8125.JPG"], gender: "women", winter: false, newIn: false, color: "black" },
+    { name: "Elegant Eyewear", price: 119, images: ["IMG_8125.JPG", "IMG_8125.JPG", "IMG_8125.JPG"], gender: "women", winter: false, newIn: false, color: "brown" },
+    { name: "Stylish Goggles", price: 129, images: ["IMG_8125.JPG", "IMG_8125.JPG", "IMG_8125.JPG"], gender: "women", winter: false, newIn: true, color: "gold" },
+    { name: "Black Sunglasses", price: 99, images: ["IMG_8125.JPG", "IMG_8125.JPG", "IMG_8125.JPG"], gender: "men", winter: true, newIn: false, color: "silver" },
+    { name: "Sporty Shades", price: 89, images: ["IMG_8125.JPG", "IMG_8125.JPG", "IMG_8125.JPG"], gender: "men", winter: true, newIn: false, color: "grey" },
+    { name: "Classic Aviators", price: 109, images: ["IMG_8125.JPG", "IMG_8125.JPG", "IMG_8125.JPG"], gender: "unisex", winter: true, newIn: true, color: "blue" }
+];
