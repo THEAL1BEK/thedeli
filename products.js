19864a28a865e81c855cc9a767881db5366132c5
THEKURDEL1 <qypshaq_all@mail.ru>
2025-09-01 19:10:04 +0600

    Update products.js

--- products.js
+++ products.js
@@ -4,7 +4,9 @@ const products = [
     { name: "Stylish Goggles", price: 129, images: ["IMG_8125.JPG", "IMG_8125.JPG", "IMG_8125.JPG"], gender: "women", winter: false, newIn: true, color: "gold" },
     { name: "Black Sunglasses", price: 99, images: ["IMG_8125.JPG", "IMG_8125.JPG", "IMG_8125.JPG"], gender: "men", winter: true, newIn: false, color: "silver" },
     { name: "Sporty Shades", price: 89, images: ["IMG_8125.JPG", "IMG_8125.JPG", "IMG_8125.JPG"], gender: "men", winter: true, newIn: false, color: "grey" },
-    { name: "Classic Aviators", price: 109, images: ["IMG_8125.JPG", "IMG_8125.JPG", "IMG_8125.JPG"], gender: "unisex", winter: true, newIn: true, color: "blue" }
+    { name: "Classic Aviators", price: 109, images: ["IMG_8125.JPG", "IMG_8125.JPG", "IMG_8125.JPG"], gender: "unisex", winter: true, newIn: true, color: "blue" },
+    { name: "Lalo", price: 138, images: ["IMG_8125.JPG", "IMG_8125.JPG", "IMG_8125.JPG"], gender: "unisex", winter: true, newIn: true, color: "red" },
+     { name: "Escape", price: 125, images: ["IMG_8125.JPG", "IMG_8125.JPG", "IMG_8125.JPG"], gender: "unisex", winter: true, newIn: true, color: "black" }
     // Добавляйте новые товары здесь, например:
     // { name: "New Item 1", price: 100, images: ["img.jpg"], gender: "unisex", winter: false, newIn: true, color: "red" },
     // { name: "New Item 2", price: 150, images: ["img.jpg"], gender: "women", winter: true, newIn: false, color: "green" }
